b911f19594f5f795c551265af65535931b76d347
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-03-12 13:19:21 -0300

    :sparkles: challenge done

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,37 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    setTasks(prevState => [
+      ...prevState,
+      {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      }
+    ]);
+
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    setTasks(
+      tasks.map(
+        task => task.id === id ? 
+          {
+            ...task,
+            isComplete: !task.isComplete,
+          }
+          : task
+      )
+    );
+
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(allTasks => allTasks.id !== id));
   }
 
   return (
